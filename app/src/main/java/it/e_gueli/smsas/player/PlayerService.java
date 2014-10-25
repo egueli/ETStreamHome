2f3b02b6ac30ed0c0b3a6d60c60de342e8f9e562
Enrico Gueli <enrico.gueli@gmail.com>
2014-10-26 00:25:10 +0200

    draft player service

---
+++ app/src/main/java/it/e_gueli/smsas/player/PlayerService.java
@@ -0,0 +1,15 @@
+package it.e_gueli.smsas.player;
+
+import org.androidannotations.annotations.EService;
+
+/**
+ * Created by ris8 on 26/10/14.
+ */
+@EService
+public class PlayerService {
+    // TODO handle media player
+    // TODO cpu-lock and wifi-lock
+    // TODO notifications
+    // TODO audio focus
+    // TODO audio becoming noisy
+}
