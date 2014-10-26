78165f17c54cb5b136ab81f8812dfaf9fd15b436
Enrico Gueli <enrico.gueli@gmail.com>
2014-10-26 02:09:14 +0200

    moved mediaplayer-related stuff in service

--- app/src/main/java/it/e_gueli/smsas/player/PlayerService.java
+++ app/src/main/java/it/e_gueli/smsas/player/PlayerService.java
@@ -2,24 +2,163 @@ package it.e_gueli.smsas.player;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.MediaController;
 
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.SftpProgressMonitor;
+
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EService;
+import org.androidannotations.annotations.UiThread;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.util.Vector;
+
+import fi.iki.elonen.SimpleWebServer;
+import it.e_gueli.smsas.sftp.InputStreamWithAvailable;
+import it.e_gueli.smsas.sftp.SftpManager;
 
 /**
  * Created by ris8 on 26/10/14.
  */
 @EService
 public class PlayerService extends Service {
-    // TODO handle media player
     // TODO cpu-lock and wifi-lock
     // TODO notifications
     // TODO audio focus
     // TODO audio becoming noisy
 
+    private static final String TAG = PlayerService.class.getSimpleName();
+
+    public class LocalBinder extends Binder {
+        public PlayerService getService() {
+            return PlayerService.this;
+        }
+    }
+
+    private final LocalBinder binder = new LocalBinder();
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return binder;
     }
+
+    private MediaController mMediaController;
+    private MediaPlayer mMediaPlayer;
+    private Handler mHandler = new Handler();
+
+    public void connectAndPlay(String songPath) {
+        buildStreaming(songPath);
+
+        mMediaPlayer = new MediaPlayer();
+        mMediaController = new MediaController(this);
+        //mMediaController.setMediaPlayer(this);
+        //mMediaController.setAnchorView(audioView);
+
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+          //              mMediaController.show(10000);
+                        mMediaPlayer.start();
+                    }
+                });
+            }
+        });
+    }
+
+    @Bean
+    SftpManager sftpManager;
+
+    SimpleWebServer server;
+
+
+    @Background
+    void buildStreaming(String songPath) {
+        try {
+            ChannelSftp channel = sftpManager.connectAndGetSftpChannel();
+
+            String dir = "";
+            String file = songPath;
+            int lastSlash = songPath.lastIndexOf(File.separator);
+            if (lastSlash != -1) {
+                dir = songPath.substring(0, lastSlash);
+                file = songPath.substring(lastSlash + 1);
+            }
+            channel.cd(dir);
+            Vector<ChannelSftp.LsEntry> entries = (Vector<ChannelSftp.LsEntry>) channel.ls(".");
+            ChannelSftp.LsEntry songEntry = null;
+            for (ChannelSftp.LsEntry entry : entries) {
+                if (entry.getFilename().toLowerCase().contains(file)) {
+                    songEntry = entry;
+                    break;
+                }
+            }
+            long size = songEntry.getAttrs().getSize();
+
+            InputStream stream = new InputStreamWithAvailable(channel.get(songEntry.getFilename(), new SftpProgressMonitor() {
+                @Override
+                public void init(int op, String src, String dest, long max) {
+                    Log.d(TAG, String.format("init() op=%d src=%s dest=%s max=%d", op, src, dest, max));
+                }
+
+                @Override
+                public boolean count(long count) {
+//                    Log.d(TAG, String.format("count() count=%d", count));
+                    return true;
+                }
+
+                @Override
+                public void end() {
+                    Log.d(TAG, "end()");
+                }
+            }), size);
+
+            if (server == null)
+                server = new SimpleWebServer("0.0.0.0", 8080, new File("/sdcard/Music"), false);
+
+            server.setHttpStream(new BufferedInputStream(stream, 131072));
+
+            if (!server.isAlive())
+                server.start();
+
+            startPlaying();
+        }
+        catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    @UiThread
+    void startPlaying() {
+        try {
+            mMediaPlayer.setDataSource("http://127.0.0.1:8080/stream.mp3");
+            mMediaPlayer.prepare();
+        }
+        catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mMediaController != null) {
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+        }
+
+
+        super.onDestroy();
+    }
+
 }
--- app/src/main/java/it/e_gueli/smsas/ui/MyActivity.java
+++ app/src/main/java/it/e_gueli/smsas/ui/MyActivity.java
@@ -1,8 +1,12 @@
 package it.e_gueli.smsas.ui;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.ServiceConnection;
 import android.media.MediaPlayer;
 import android.os.Handler;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -34,6 +38,8 @@ import java.util.Vector;
 
 import fi.iki.elonen.SimpleWebServer;
 import it.e_gueli.smsas.R;
+import it.e_gueli.smsas.player.PlayerService;
+import it.e_gueli.smsas.player.PlayerService_;
 import it.e_gueli.smsas.sftp.InputStreamWithAvailable;
 import it.e_gueli.smsas.sftp.SftpManager;
 import it.e_gueli.smsas.sftp.StoredOrAskedUserInfo;
@@ -43,13 +49,11 @@ import it.e_gueli.smsas.songdb.Song;
 
 @EActivity(R.layout.activity_my)
 @OptionsMenu(R.menu.my)
-public class MyActivity extends Activity implements MediaController.MediaPlayerControl {
+public class MyActivity extends Activity {
 
     private static final String TAG = MyActivity.class.getSimpleName();
 
-    private MediaController mMediaController;
-    private MediaPlayer mMediaPlayer;
-    private Handler mHandler = new Handler();
+
 
     @Bean
     StoredOrAskedUserInfo userInfo;
@@ -75,19 +79,40 @@ public class MyActivity extends Activity implements MediaController.MediaPlayerC
     @OrmLiteDao(helper = DatabaseHelper.class, model = Song.class)
     RuntimeExceptionDao<Song, Integer> songDao;
 
-    SimpleWebServer server;
+    private PlayerService playerService;
+    private ServiceConnection playerServiceConnection;
 
     @AfterViews
     @UiThread
     void setupSearchBox() {
+        searchBox.setEnabled(false); // will be enabled when the player service is bound
         searchBox.setAdapter(searchBoxAdapter);
         searchBox.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Song song = songDao.queryForId((int)id);
-                connectAndPlay(song.getFullPath());
+                playerService.connectAndPlay(song.getFullPath());
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        playerServiceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                playerService = ((PlayerService.LocalBinder) iBinder).getService();
+                searchBox.setEnabled(true);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                searchBox.setEnabled(false);
+            }
+        };
+        bindService(PlayerService_.intent(this).get(), playerServiceConnection, Context.BIND_AUTO_CREATE);
     }
 
     @OptionsItem(R.id.action_settings)
@@ -127,166 +152,81 @@ public class MyActivity extends Activity implements MediaController.MediaPlayerC
         statusText.setText(message);
     }
 
-    //@AfterInject
-    void connectAndPlay(String songPath) {
-        buildStreaming(songPath);
 
-        mMediaPlayer = new MediaPlayer();
-        mMediaController = new MediaController(this);
-        mMediaController.setMediaPlayer(this);
-        mMediaController.setAnchorView(audioView);
-
-        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mMediaController.show(10000);
-                        mMediaPlayer.start();
-                    }
-                });
-            }
-        });
-    }
-
-    @UiThread
-    void startPlaying() {
-        try {
-            mMediaPlayer.setDataSource("http://127.0.0.1:8080/stream.mp3");
-            mMediaPlayer.prepare();
+    @Override
+    protected void onStop() {
+        if (playerServiceConnection != null) {
+            unbindService(playerServiceConnection);
+            playerService = null;
+            playerServiceConnection = null;
         }
-        catch (Exception e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        }
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mMediaPlayer.stop();
-        mMediaPlayer.release();
-    }
-
-    @Background
-    void buildStreaming(String songPath) {
-        try {
-            ChannelSftp channel = sftpManager.connectAndGetSftpChannel();
-
-            String dir = "";
-            String file = songPath;
-            int lastSlash = songPath.lastIndexOf(File.separator);
-            if (lastSlash != -1) {
-                dir = songPath.substring(0, lastSlash);
-                file = songPath.substring(lastSlash + 1);
-            }
-            channel.cd(dir);
-            Vector<ChannelSftp.LsEntry> entries = (Vector<ChannelSftp.LsEntry>) channel.ls(".");
-            ChannelSftp.LsEntry songEntry = null;
-            for (ChannelSftp.LsEntry entry : entries) {
-                if (entry.getFilename().toLowerCase().contains(file)) {
-                    songEntry = entry;
-                    break;
-                }
-            }
-            long size = songEntry.getAttrs().getSize();
-
-            InputStream stream = new InputStreamWithAvailable(channel.get(songEntry.getFilename(), new SftpProgressMonitor() {
-                @Override
-                public void init(int op, String src, String dest, long max) {
-                    Log.d(TAG, String.format("init() op=%d src=%s dest=%s max=%d", op, src, dest, max));
-                }
-
-                @Override
-                public boolean count(long count) {
-//                    Log.d(TAG, String.format("count() count=%d", count));
-                    return true;
-                }
-
-                @Override
-                public void end() {
-                    Log.d(TAG, "end()");
-                }
-            }), size);
-
-            if (server == null)
-                server = new SimpleWebServer("0.0.0.0", 8080, new File("/sdcard/Music"), false);
-
-            server.setHttpStream(new BufferedInputStream(stream, 131072));
-
-            if (!server.isAlive())
-                server.start();
-
-            startPlaying();
-        }
-        catch (Exception e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        }
+        super.onStop();
     }
 
 
-    @Override
-    public boolean canPause() {
-        return true;
-    }
-
-    @Override
-    public boolean canSeekBackward() {
-        return false;
-    }
-
-    @Override
-    public boolean canSeekForward() {
-        return false;
-    }
-
-    @Override
-    public int getBufferPercentage() {
-        int percentage = (mMediaPlayer.getCurrentPosition() * 100) / mMediaPlayer.getDuration();
-
-        return percentage;
-    }
-
-    @Override
-    public int getCurrentPosition() {
-        return mMediaPlayer.getCurrentPosition();
-    }
-
-    @Override
-    public int getDuration() {
-        return mMediaPlayer.getDuration();
-    }
-
-    @Override
-    public boolean isPlaying() {
-        return mMediaPlayer.isPlaying();
-    }
-
-    @Override
-    public void pause() {
-        if(mMediaPlayer.isPlaying())
-            mMediaPlayer.pause();
-    }
-
-    @Override
-    public void seekTo(int pos) {
-        mMediaPlayer.seekTo(pos);
-    }
-
-    @Override
-    public void start() {
-        mMediaPlayer.start();
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        //mMediaController.show();
-
-        return false;
-    }
-
-    @Override
-    public int getAudioSessionId() {
-        return 0;
-    }
+//    @Override
+//    public boolean canPause() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean canSeekBackward() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean canSeekForward() {
+//        return false;
+//    }
+//
+//    @Override
+//    public int getBufferPercentage() {
+//        int percentage = (mMediaPlayer.getCurrentPosition() * 100) / mMediaPlayer.getDuration();
+//
+//        return percentage;
+//    }
+//
+//    @Override
+//    public int getCurrentPosition() {
+//        return mMediaPlayer.getCurrentPosition();
+//    }
+//
+//    @Override
+//    public int getDuration() {
+//        return mMediaPlayer.getDuration();
+//    }
+//
+//    @Override
+//    public boolean isPlaying() {
+//        return mMediaPlayer.isPlaying();
+//    }
+//
+//    @Override
+//    public void pause() {
+//        if(mMediaPlayer.isPlaying())
+//            mMediaPlayer.pause();
+//    }
+//
+//    @Override
+//    public void seekTo(int pos) {
+//        mMediaPlayer.seekTo(pos);
+//    }
+//
+//    @Override
+//    public void start() {
+//        mMediaPlayer.start();
+//    }
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        //mMediaController.show();
+//
+//        return false;
+//    }
+//
+//    @Override
+//    public int getAudioSessionId() {
+//        return 0;
+//    }
 }
